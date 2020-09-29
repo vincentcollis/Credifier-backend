2a302197dd59eaebac095e370fbe49c46b54d4db
vincentcollis <vinsanity0050@gmail.com>
2020-09-29 15:41:54 -0400

    added validation to Rating class to ensure user cannnot rate the same review multiple times

--- app/models/rating.rb
+++ app/models/rating.rb
@@ -1,4 +1,6 @@
 class Rating < ApplicationRecord
+    validates :rater_id, uniqueness: {scope: :review_id, message: "User cannot rate a review more then once!"}
+
     belongs_to :review
     belongs_to :rater, class_name: "User", foreign_key: "rater_id"
 end
